36a0a4cea45a07f3ec3b8f2f5fac0bd9c30b9242
cmarro3 <christophermarroquin352@gmail.com>
2022-02-10 08:59:38 -0600

    PHP stuff

---
+++ hello.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Demo</title>
+</head>
+<body>
+<p><?php echo 'hello world';?></p>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+    echo 'Hello world';
+    echo '<br>';
+   // <?='hello world' writing like this is no recommended
+  // write like this <? echo 'Hello world'; etc
+
+//variables
+// echo '<br>'; this just breaks lines
+$age = 34;
+echo '<br>';
+echo "my age is: ";
+echo $age;
+echo '<br>';
+$double_age = $age *2;
+echo '<br>';
+echo $double_age;
+echo '<br>';
+const version = 5.5;
+echo '<br>';
+echo version;
+
+$name = "Christopher";
+$first_letter = 'C';
+echo '<br>';
+echo $name;
+echo '<br>';
+echo $first_letter;
+echo '<title>PHP Demo</title>';
+echo '<br>';
+echo 'Welcome to ' .$name . ' website';
+echo '<br>';
+echo 'welcome to Christopher\'s website';
+echo '<br>';
+
+
+
+
